791bbb83810a8867d507e5737892764352b0a3db
tuannv0069 <tuannv0069@gmail.com>
2022-03-06 17:37:26 +0700

    update config circleci

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Automatic deployment 2");
+  res.send("Automatic deployment 3");
 });
 
 app.listen(port, () => {
